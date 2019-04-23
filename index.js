4f7f3ddb98c4aa5e48f38ca206a6acdc63784976
Christian Maioli (@amazedcoder) <mmchristian@gmail.com>
2019-04-23 19:05:27 +0000

    feat: some checks for permissions

--- index.js
+++ index.js
@@ -5,11 +5,19 @@ const TOML = require('@iarna/toml');
 const CONFIG_PATH = 'reqs.toml';
 
 var isConfig = fs.existsSync(CONFIG_PATH);
+const results = [];
 
 if (isConfig) {
   const config = TOML.parse(fs.readFileSync(CONFIG_PATH));
 
-  console.log('loaded config');
+  var files = config.files || [];
+
+  if (files.readable) {
+      files.readable.forEach(function (f) {
+          if (fs.accessSync(f, fs.constants.R_OK))
+      });
+  }
+
   console.log(config);
 } else {
   console.log('no config found');
