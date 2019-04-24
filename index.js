e609321c07d5f6fb94320ff40769e09349baaad8
Christian Maioli (@amazedcoder) <mmchristian@gmail.com>
2019-04-24 01:19:37 +0000

    feat: basic checks for file reading and writing

--- index.js
+++ index.js
@@ -13,12 +13,37 @@ if (isConfig) {
   var files = config.files || [];
 
   if (files.readable) {
-      files.readable.forEach(function (f) {
-          if (fs.accessSync(f, fs.constants.R_OK))
-      });
+    files.readable.forEach(checkReadable);
   }
-
-  console.log(config);
+  if (files.writeable) {
+    files.writeable.forEach(checkWriteable);
+  }
 } else {
-  console.log('no config found');
+  logStatus(false, 'Configuration not found');
+}
+
+function logStatus (check, text) {
+  const green = '\033[32m';
+  const red = '\033[31m';
+  console.log((check ? green + '+ ' : red + '- ') + text);
+}
+
+function checkReadable (f) {
+  try {
+    fs.accessSync(f, fs.constants.R_OK)
+    var readable = true;
+  } catch (err) {
+    var readable = false;
+  }
+  logStatus(readable, 'Path ' + f + ' is ' + (!readable ? 'not ' : '') + 'readable');
+}
+
+function checkWriteable (f) {
+  try {
+    fs.accessSync(f, fs.constants.W_OK)
+    var writeable = true;
+  } catch (err) {
+    var writeable = false;
+  }
+  logStatus(writeable, 'Path ' + f + ' is ' + (!writeable ? 'not ' : '') + 'writeable');
 }
